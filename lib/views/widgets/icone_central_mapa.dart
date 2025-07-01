905c080514c58cffb0a61648704f5e9fbf66f236
adalbertocarv <adalbertosantu@gmail.com>
2025-07-01 12:30:12 -0300

    -logica do botão aparece apenas no modo cadastro

--- lib/views/widgets/icone_central_mapa.dart
+++ lib/views/widgets/icone_central_mapa.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../controllers/modo_app_controller.dart';
 
 class IconeCentralMapa extends StatelessWidget {
-  // Propriedade para receber o estado do tema
-  final bool isDarkMode;
 
-  // Construtor que exige o 'isDarkMode'
+
   const IconeCentralMapa({
     super.key,
-    required this.isDarkMode,
   });
 
   @override
   Widget build(BuildContext context) {
-    // Toda a lógica do Stack que criamos antes agora vive aqui dentro.
-    // Ele é totalmente independente e só se preocupa em se desenhar.
+    final modoApp = context.watch<ModoAppController>();
+
+    if (!modoApp.isCadastro) {
+      return const SizedBox.shrink();
+    }
+
     return IgnorePointer(
       child: Center(
         child: Stack(
@@ -37,9 +40,6 @@ class IconeCentralMapa extends StatelessWidget {
               offset: const Offset(0, -20),
               child: Icon(
                 Icons.location_pin,
-                // A cor agora usa a propriedade 'isDarkMode' que foi passada.
-                // No seu exemplo, a cor era vermelha para ambos os modos.
-                // Se quisesse cores diferentes, seria: isDarkMode ? Colors.white : Colors.black
                 color: Colors.red,
                 size: 40,
               ),
