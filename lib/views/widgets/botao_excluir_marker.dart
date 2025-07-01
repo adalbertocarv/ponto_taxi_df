6597cced6b3b777ae53ec4a877739e4efb168af2
adalbertocarv <adalbertosantu@gmail.com>
2025-07-01 11:45:21 -0300

    -botão excluir pontos cadastrados temporariamente

---
+++ lib/views/widgets/botao_excluir_marker.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../controllers/mapa_controller.dart';
+import '../../providers/themes/tema_provider.dart';
+
+class BotaoExcluirMarker extends StatefulWidget {
+  const BotaoExcluirMarker({super.key});
+
+  @override
+  State<BotaoExcluirMarker> createState() => _BotaoExcluirMarkerState();
+}
+
+class _BotaoExcluirMarkerState extends State<BotaoExcluirMarker> {
+  @override
+  Widget build(BuildContext context) {
+    final mapaController = context.watch<MapaController>();
+    final themeProvider = context.watch<ThemeProvider>();
+
+    // Se o ícone não for visível, retorna um widget vazio (não exibe o botão)
+    if (mapaController.iconeVisivel) {
+      return SizedBox.shrink();  // Retorna um widget vazio, garantindo que o tipo não seja nulo
+    }
+
+    // Caso o ícone esteja visível, retorna o botão
+    return Positioned(
+      top: 160,
+      left: 16,
+      child: FloatingActionButton.small(
+        tooltip: 'Excluir Marcador',
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        backgroundColor: themeProvider.primaryColor,
+          onPressed: () {
+          mapaController.limparMarkers();
+        },
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
